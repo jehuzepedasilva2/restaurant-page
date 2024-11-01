8319f7b8a32ea76a0d023b449e3051f1573e8370
Jehu Zepeda-Silva <jehuzepeda@yahoo.com>
2024-10-31 19:53:44 -0700

    fixed image background to fixed so that image doesnt move with scrolling

---
+++ src/contact.js
@@ -0,0 +1,19 @@
+import CachedDOM from './cachedDOM.js';
+
+function contactPage() {
+  const contentContainer = CachedDOM.getContentContainer();
+
+  const ourLocationContainer = document.createElement("div");
+  ourLocationContainer.classList.add("location-container");
+
+  const locationTitle = document.createElement("h1");
+  locationTitle.classList.add("location-title");
+  locationTitle.textContent = 'Our Location';
+  ourLocationContainer.appendChild(locationTitle);
+
+  
+
+  contentContainer.appendChild(ourLocationContainer);
+}
+
+export default contactPage;
--- src/script.js
+++ src/script.js
@@ -1,12 +1,11 @@
 // import './home-styles.css';
 // import HomePage from "./home.js";
-
 // HomePage();
-// import './menu-styles.css';
-// import MenuPage from './menu.js';
 
-// MenuPage();
-import './contact-styles.css';
-import ContactPage from './contact.js';
+import './menu-styles.css';
+import MenuPage from './menu.js';
+MenuPage();
 
+// import './contact-styles.css';
+// import ContactPage from './contact.js';
 ContactPage();
