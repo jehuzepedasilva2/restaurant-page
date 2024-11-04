f8101f67429d78c171dc8831f2194368d7fe57f9
Jehu Zepeda-Silva <jehuzepeda@yahoo.com>
2024-11-04 10:44:56 -0800

    split into dev/prod.common added scripts to package.json file

--- webpack.common.js
+++ webpack.common.js
@@ -2,16 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin"); //not sure what either of these do?
 
 module.exports = {
-  mode: 'production', // there is production, development, and none
+  // mode: 'production', // there is production, development, and none
   entry: './src/script.js', // entry point, this is where all other js files will be imported to and used within
   output: {
     filename: 'main.js', // the name of the file that will be outputted in dist
     path: path.resolve(__dirname, 'dist'), // the name of folder, by convention its called dist (distribution), not sure what resolve(..) does
     clean: true, // saves in dist and "deletes old files and replaces with current (autosave and clean up?)"
-  },
-  devtool: "eval-source-map", // helps in matching up error msgs to correct files and lines from our development code (src)
-  devServer: { // by default webpack will only watch changes of bundled .js files and ignore html, this changes that.
-    watchFiles: ["./src/template.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
